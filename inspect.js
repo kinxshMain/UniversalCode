84a3d8188a318e864bc526ab7a062bd68d55c9af
kinxshMain <114166303+kinxshMain@users.noreply.github.com>
2023-01-26 11:25:58 -0500

    Create inspect.js

---
+++ inspect.js
@@ -0,0 +1,1 @@
+javascript: if (document.designMode = "off") {console.log(document.designMode = "on"); window.alert("INSPECT: ON");} else {console.log(document.designMode = "off"); window.alert("INSPECT: OFF");}
