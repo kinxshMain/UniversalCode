42aa7ca69aee7ebd225c312c2962d9b3b9392c6d
420hyper <123647937+420hyperspace@users.noreply.github.com>
2023-01-30 09:28:39 -0500

    Create funnyquestion2.js

---
+++ Bookmarklets/Funny Questions/funnyquestion2.js
@@ -0,0 +1,1 @@
+javascript: window.alert("hello"); deepball = prompt("want free robux?"); if (deepball == "no") {window.alert("what is wrong with you how can you not want free robux");} if (deepball == "yes") {window.alert("haha no robux for you stay poor i cant believe you tried to get free robux lolololol");}
